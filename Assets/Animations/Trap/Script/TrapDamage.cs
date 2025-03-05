5d2beeb45fed518c5b9b52c8c8f20e58cf2ccf19
nam14012003 <140016784+nam14012003@users.noreply.github.com>
2025-03-06 06:28:09 +0700

    Map1

---
+++ Assets/Animations/Trap/Script/TrapDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    public int damage = 10; 
+    public string targetTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Kiểm tra xem object va chạm có tag "Player" không
+        if (collision.CompareTag(targetTag))
+        {
+            CharacterStats characterStats = collision.GetComponent<CharacterStats>();
+            if (characterStats != null)
+            {
+                characterStats.TakeDamage(damage);
+                Debug.Log("Player bị dính bẫy! Gây " + damage + " sát thương.");
+            }
+        }
+    }
+}
